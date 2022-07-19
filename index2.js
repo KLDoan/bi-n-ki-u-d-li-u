bd60bb00f1d3202b3bf9306eb4f7d3cb6dd7f6c4
unknown <longquydoan2020@gmail.com>
2022-07-19 19:00:22 +0700

    ok

---
+++ index2.js
@@ -0,0 +1,11 @@
+// //Cach 1: prompt
+// // let name = prompt ("Moi ban nhap ten")
+// // prompt hien thi mot thong bao cho nguoi dung nhap vao
+// //Cach 2: confirm true false
+// // let gt = confirm ("Ban muon truy cap vao trang web nay?")
+// // khi bam ok, gia tri tra ve cho may chu la true, sau do se vao duoc web tiep
+
+// document.getElementById("demo2").innerHTML = <h1>Xin chao</h1>
+
+// // the trong form
+// console.log ("hello ban")
---
+++ nhap du lieu .js
@@ -0,0 +1,2 @@
+let name = prompt("Moi ban nhap ten")
+document.getElementById("demo_1").innerText = name 
