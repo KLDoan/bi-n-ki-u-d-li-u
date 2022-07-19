4aa396fe602df04b359ccf8d679c6dbfab50f137
unknown <longquydoan2020@gmail.com>
2022-07-19 19:52:33 +0700

    area

---
+++ toantu.js
@@ -0,0 +1,8 @@
+let Width;
+let Height;
+Width = prompt("Enter the width");
+Height = prompt("Enter the height");
+let width = parseInt(Width);
+let height = parseInt(Height);
+let area = width * height;
+document.write("The area is: " + area);
