8b3579de2fe96c0a6e38db6e96812e0806b0a852
unknown <longquydoan2020@gmail.com>
2022-07-19 20:24:20 +0700

    bank

---
+++ bank.js
@@ -0,0 +1,8 @@
+let Vay = prompt("Hay nhap vao so tien vay ")
+let LaiSuat = prompt("Hay nhap vao so lai suat hang nam ")
+let Nam = prompt("Hay nhap vao so nam ban muon vay ")
+let vay = parseInt(Vay);
+let lai = parseInt(LaiSuat)
+let nam = parseInt(Nam)
+pay = vay + nam * lai * vay
+alert("ban phai tra " + pay + " sau " + Nam + " nam")
