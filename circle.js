0265dcb59bbab5024bd6c64331b3380ba9417e23
unknown <longquydoan2020@gmail.com>
2022-07-19 20:10:38 +0700

    circle

---
+++ circle.js
@@ -0,0 +1,5 @@
+let R = prompt("Hay nhap ban kinh hinh tron (don vi: cm)")
+C = 2*R*3.14
+S = R*R*3.14
+alert("Chu vi hinh tron co ban kinh ban da nhap la " + C) 
+alert("Dien tich hinh tron co ban kinh ban da nhap la " + S) 
